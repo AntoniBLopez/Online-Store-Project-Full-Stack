573e21f8c22da1be01b10f73af5c0a884320d9d9
Toni B.Lopez local <tonibassols@outlook.es>
2022-04-07 11:19:08 +0200

    I add the configuration to work with Sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,56 @@
-// Este archivo es clave para trabajar con nuestro proyecto
-
-const path = require("path"); // Nos permite saber dónde vamos a estar utilizando el proyecto
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // traemos el plugin que hemos instalado de html con webpack
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // traemos la dependencia que hemos instalado
 
 module.exports = {
-    entry: './src/index.js', // aquí indicamos el punto de entrada de nuestra APP
-    output: { // aquí indicamos dónde va a estar viviendo el proyecto una vez esté preparado
-        path: path.resolve(__dirname, "dist"), // tenemos __dirname para saber dónde me encuentro, y como segundo parámetro -->
-        // pasamos la carpeta que vamos a utilizar "dist" ( de distribución )
-        filename: 'bundle.js'// Le ponemos un nombre al empaquetado que se va a crear
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'bundle.js'
     },
-    mode: 'development', // puede ser development, production o none
-    resolve: { // aquí añado las extensiones por medio de un array
+    mode: 'development',
+    resolve: {
         extensions: ['.js', '.jsx']
     },
-    module: { // aquí añadimos las reglas que vamos a crear con nuestros loaders y los plugins
+    module: {
         rules: [
             {
-                test: /\.(js|jsx)$/, // es un reject al que le indicamos que detecte las extensiones js y jsx para -->
-                // poder trabjar con nuestro recurso
-                exclude: /node_modules/, // excluimos lo que no queremos que leea
-                use: { // añadimos lo que vamos a utilzar
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
                     loader: 'babel-loader'
                 }
             },
-            { // ahora vamos a crear una regla que nos va a permitir trabajar con HTML
-                test: /\.html$/, // le decimos que lea todos los archivos HTML
+            {
+                test: /\.html$/,
                 use: [
                     {
-                        loader: 'html-loader' // le decimos que va a trabjar con html-loader
+                        loader: 'html-loader'
                     }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i, // en [ac] lo que estamos diciendo es que puede ser una letra u otra letra, y la i del final no sé que es
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
                 ]
             }
         ]
     },
-    plugins: [ // aquí agregamos los plugins que vamos a estar utilizando
+    plugins: [
         new HtmlWebpackPlugin({
-            template: './public/index.html', // le decimos que vamos a utilizar un template y le indicamos dónde está
-            filename: './index.html' // indicamos el nombre del empaquetado
-        })
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({ // añadimos un nuevo plugin
+            filename: '[name].css'
+        }),
     ],
+    devServer: { // me permite trabajar con todos los elementos que son clave para el entorno local de desarrollo
+        allowedHosts: path.join(__dirname, 'dist'), // permite saber dónde está trabajando el proyecto ('dist' de distribution)
+        compress: true,
+        port: 3005, // y el puerto en el que vamos a trabajar
+    }
 }
-
-// Y ya tenemos la estructura del webpack para el pryecto de la tienda online
